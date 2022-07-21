440de50c5c0cea06d0655c1b9fe57199c306e39e
owenwahlgren <owenwahlgren@Owens-MBP.cable.rcn.com>
2022-07-21 18:16:27 -0400

    storage efficiency, deposit steps and limit

--- src/Jackpot.sol
+++ src/Jackpot.sol
@@ -3,22 +3,18 @@ import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-
 /*
     @title EVM.GG JACKPOT
     @author owen.eth
     @notice A Chainlink VRF Jackpot game. 
-
-    TODO:
-        * Configure deposit limit
-        * Reward spiner() caller?
-        
 */
+
 contract Jackpot is VRFConsumerBaseV2 {
 
-    /* Chainlink settings */
-    /* see https://docs.chain.link/docs/vrf-contracts/#configurations */
-
+    /* 
+    @dev Chainlink settings
+    @notice see https://docs.chain.link/docs/vrf-contracts/#configurations 
+    */
     VRFCoordinatorV2Interface COORDINATOR;
     LinkTokenInterface LINKTOKEN;
     address constant link_token_contract = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
@@ -28,9 +24,9 @@ contract Jackpot is VRFConsumerBaseV2 {
     uint32 constant callbackGasLimit = 100000;
     uint16 constant requestConfirmations = 3;
     uint32 constant numWords =  1;
-    uint256 constant chainlink_fee = 0.25 ether; //.25 link
-
+    uint256 constant chainlink_fee = 0.25 ether; //.25 link fee for oracle
     /*
+
     @dev Initalize contract with Chainlink VRF subscription information
     @param
         * uint64 subscriptionId => Chainlink subscription id
@@ -45,40 +41,44 @@ contract Jackpot is VRFConsumerBaseV2 {
         vrfCoordinator = vrf;
         keyHash = keyhash;
     }
-    
-    /* Jackpot settings */
 
+    /* 
+        @dev Jackpot settings 
+        @notice Casting block.timestamp to uint32 is valid until the year 2106
+             * Cutting our gas costs at the expense of the future generation 😎 *
+    */
     struct GAME {
        address winner;
-       uint256 amount;
-       uint256 timeBegin;
-       uint256 timeEnd;
+       uint96 amount;
+       uint32 timeBegin;
+       uint32 timeEnd;
        bool active;
     }
-
     mapping(uint256 => GAME) public history;
+    uint256 constant public DEPOSIT_STEP = 0.01 ether;
     uint256 constant public DEPOSIT_LIMIT = 1 ether;
-    uint256 constant public JACKPOT_TIMER = 1 minutes;
+    uint32 constant public JACKPOT_TIMER = 2 minutes;
     uint256 public currGameId;
-    uint256 public jackpotTotal;
+    uint256 public jackpotTotalTickets;
     address payable[] public jackpotTickets;
-
 
     /*
     @dev Deposit to the jackpot
+        * Require msg.value is within deposit restrictions (DEPOSIT_STEP <= msg.value <= DEPOSIT LIMIT)
         * Require game is active and block.timestamp is before game expiry
         * Start countdown of a new game if one doesn't exist
-        * Allocate tickets for msg.sender equivelant to msg.value
+        * Allocate tickets for msg.sender in steps of DEPOSIT_STEP in equivelance to msg.value (ie 1 eth == 100 tickets)
     */
     function deposit() public payable {
+        require(msg.value <= DEPOSIT_LIMIT && msg.value >= DEPOSIT_STEP, "Deposit is less or more than the deposit restrictions");
         GAME memory game = history[currGameId];
         if (game.active) {require(block.timestamp < game.timeEnd, "Timer expired, waiting for spin to be called");}
-        else {game.active = true; game.timeBegin = block.timestamp; game.timeEnd = block.timestamp + JACKPOT_TIMER;}
+        else {game.active = true; game.timeBegin = uint32(block.timestamp); game.timeEnd = uint32(block.timestamp) + JACKPOT_TIMER;}
 
-        game.amount += msg.value;
-        for(uint i = 0; i < msg.value; i++) {
-            jackpotTickets[jackpotTotal] = payable(msg.sender);
-            jackpotTotal += 1;
+        game.amount += uint96(msg.value);
+        for(uint i = 0; i < msg.value / DEPOSIT_STEP; i++) {
+            jackpotTickets[jackpotTotalTickets] = payable(msg.sender);
+            jackpotTotalTickets += 1;
         }
     }
 
@@ -87,6 +87,8 @@ contract Jackpot is VRFConsumerBaseV2 {
         * Require the game exists and the timer has expired
         * Give caller SPIN_REWARD % of the pot to promote autonomous function calls
         * Request random number from COORDINATOR 
+        TODO: Decide if call will be funded by the contract, user, or subscription service
+        TODO: Reward spin caller?
     */
     function spin() public {
         GAME memory game = history[currGameId];
@@ -114,15 +116,13 @@ contract Jackpot is VRFConsumerBaseV2 {
         uint256,
         uint256[] memory randomWords
     ) internal override {
-        uint256 result = (randomWords[0] % jackpotTotal) + 1; 
-        address payable winner = jackpotTickets[result];
+        uint256 winningTicket = (randomWords[0] % jackpotTotalTickets) + 1; 
+        address payable winner = jackpotTickets[winningTicket];
         history[currGameId].active = false;
         history[currGameId].winner = winner;
+        winner.transfer(uint256(history[currGameId].amount));
         currGameId += 1;
-        winner.transfer(jackpotTotal);
-        jackpotTotal = 0;
+        jackpotTotalTickets = 0;
         delete jackpotTickets;
     }
-
-
 }
