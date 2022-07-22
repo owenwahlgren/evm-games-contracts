8497f05876fa0dccf819ceb1c4d085d8e7b0c432
owenwahlgren <owenwahlgren@Owens-MBP.cable.rcn.com>
2022-07-22 17:41:01 -0400

    gas, vrf security

--- src/Jackpot.sol
+++ src/Jackpot.sol
@@ -17,14 +17,14 @@ contract Jackpot is VRFConsumerBaseV2 {
     */
     VRFCoordinatorV2Interface COORDINATOR;
     LinkTokenInterface LINKTOKEN;
-    address constant link_token_contract = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
-    uint64 s_subscriptionId;
-    address vrfCoordinator;
-    bytes32 keyHash;
+    uint64 immutable s_subscriptionId;
+    bytes32 constant keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    address constant vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab; // rinkeby
+    address public constant link_token_contract = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
     uint32 constant callbackGasLimit = 100000;
     uint16 constant requestConfirmations = 3;
     uint32 constant numWords =  1;
-    uint256 constant chainlink_fee = 0.25 ether; //.25 link fee for oracle
+    uint256 constant chainlink_fee = 0.25 ether; //.25 link fee for mainnet
 
     /*
         @dev Initalize contract with Chainlink VRF subscription information
@@ -34,12 +34,11 @@ contract Jackpot is VRFConsumerBaseV2 {
             * bytes32 keyhash =>  Gaslane configuration
         @notice see https://vrf.chain.link/
     */
-    constructor(uint64 subscriptionId, address vrf, bytes32 keyhash) VRFConsumerBaseV2(vrfCoordinator) {
+    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
         COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
         LINKTOKEN = LinkTokenInterface(link_token_contract);
         s_subscriptionId = subscriptionId;
-        vrfCoordinator = vrf;
-        keyHash = keyhash;
+
     }
 
     /* 
@@ -55,6 +54,7 @@ contract Jackpot is VRFConsumerBaseV2 {
        bool active;
     }
     mapping(uint256 => GAME) public history;
+    mapping(uint256 => bool) public vrfRequested;
     uint256 constant public DEPOSIT_STEP = 0.01 ether;
     uint256 constant public DEPOSIT_LIMIT = 1 ether;
     uint32 constant public JACKPOT_TIMER = 2 minutes;
@@ -70,16 +70,17 @@ contract Jackpot is VRFConsumerBaseV2 {
             * Allocate tickets for msg.sender in steps of DEPOSIT_STEP in equivelance to msg.value (ie 1 eth == 100 tickets)
     */
     function deposit() public payable {
-        require(msg.value <= DEPOSIT_LIMIT && msg.value >= DEPOSIT_STEP, "Deposit is less or more than the deposit restrictions");
+        require(msg.value <= DEPOSIT_LIMIT && msg.value >= DEPOSIT_STEP, "Deposit violates restrictions restrictions");
         GAME memory game = history[currGameId];
         if (game.active) {require(uint32(block.timestamp) < game.timeEnd, "Timer expired, waiting for spin to be called");}
         else {game.active = true; game.timeBegin = uint32(block.timestamp); game.timeEnd = uint32(block.timestamp) + JACKPOT_TIMER;}
 
-        game.amount += uint96(msg.value);
-        for(uint i = 0; i < msg.value / DEPOSIT_STEP; i++) {
-            jackpotTickets[jackpotTotalTickets] = payable(msg.sender);
-            jackpotTotalTickets += 1;
+        uint ticketsAllocated = msg.value / DEPOSIT_STEP;
+        for(uint i = 0; i < ticketsAllocated; i++) {
+            jackpotTickets[jackpotTotalTickets + i] = payable(msg.sender);
         }
+        jackpotTotalTickets += ticketsAllocated;
+        game.amount += uint96(msg.value);
     }
 
     /*
@@ -92,7 +93,8 @@ contract Jackpot is VRFConsumerBaseV2 {
     */
     function spin() public {
         GAME memory game = history[currGameId];
-        require(game.active == true && uint32(block.timestamp) >= game.timeEnd, "Timer not expired");
+        require(!vrfRequested[currGameId] && uint32(block.timestamp) >= game.timeEnd, "Timer not expired");
+        vrfRequested[currGameId] = true;
         LINKTOKEN.transferAndCall(address(COORDINATOR), chainlink_fee, abi.encode(s_subscriptionId));
         COORDINATOR.requestRandomWords(
             keyHash,
@@ -116,11 +118,12 @@ contract Jackpot is VRFConsumerBaseV2 {
         uint256,
         uint256[] memory randomWords
     ) internal override {
-        uint256 winningTicket = (randomWords[0] % jackpotTotalTickets) + 1; 
+        uint256 winningTicket = (randomWords[0] % jackpotTotalTickets); 
         address payable winner = jackpotTickets[winningTicket];
-        history[currGameId].active = false;
-        history[currGameId].winner = winner;
-        winner.transfer(uint256(history[currGameId].amount));
+        GAME memory game = history[currGameId];
+        game.active = false;
+        game.winner = winner;
+        winner.transfer(uint256(game.amount));
         currGameId += 1;
         jackpotTotalTickets = 0;
         delete jackpotTickets;
